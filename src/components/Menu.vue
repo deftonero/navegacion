f4948656262784494dba22aa1db861d328b1441f
deftonero <pabp86@hotmail.com>
2022-05-19 11:54:24 -0500

    menu componente

---
+++ src/components/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <div id="home" class="fondo home">hola soy home</div>
+    <div id="about" class="fondo about">hola soy about</div>
+    <div id="contact" class="fondo home">hola soy contact</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Menu from "../components/Menu.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    Menu,
+  },
+};
 </script>
+<style scoped>
+.fondo {
+  height: 100vh;
+  color: azure;
+}
+.home {
+  background-color: #bc1212;
+}
+.about {
+  background-color: #12bc12;
+}
+</style>
